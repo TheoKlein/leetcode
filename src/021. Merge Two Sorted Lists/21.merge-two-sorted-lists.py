9ca7642ca9f9538114703f26f7a6e88a271e8465
TheoKlein <theokleintw@gmail.com>
2019-02-19 23:24:32 +0800

    update "21.merge-two-sorted-lists.py"

--- src/021. Merge Two Sorted Lists/21.merge-two-sorted-lists.py
+++ src/021. Merge Two Sorted Lists/21.merge-two-sorted-lists.py
@@ -29,6 +29,20 @@
 
 class Solution:
     def mergeTwoLists(self, l1: 'ListNode', l2: 'ListNode') -> 'ListNode':
+        """遞迴 recursive
+        """
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next =  self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+    def mergeTwoLists_iteratively(self, l1: 'ListNode', l2: 'ListNode') -> 'ListNode':
+        """迭代 iterative
+        """
         merged = cur = ListNode(0)
         while l1 and l2:
             if l1.val < l2.val:
